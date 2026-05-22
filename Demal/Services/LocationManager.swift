514c3590cb53f2435d9f8c65288f79a3ccaef288
Alikhan Amangeldiyev <agalikhan@gmail.com>
2026-05-22 23:36:25 +0500

    +LocationManager

---
+++ Demal/Services/LocationManager.swift
@@ -0,0 +1,39 @@
+// Services/LocationManager.swift
+// TazaAua – CoreLocation manager for current GPS updates
+
+import CoreLocation
+import Foundation
+import Combine
+
+final class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+    @Published private(set) var currentLocation: CLLocation? = nil
+
+    private let manager = CLLocationManager()
+
+    override init() {
+        super.init()
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+    }
+
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        switch manager.authorizationStatus {
+        case .authorizedAlways, .authorizedWhenInUse:
+            manager.startUpdatingLocation()
+        case .notDetermined:
+            manager.requestWhenInUseAuthorization()
+        default:
+            manager.stopUpdatingLocation()
+        }
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        currentLocation = locations.last
+    }
+
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        manager.stopUpdatingLocation()
+    }
+}
